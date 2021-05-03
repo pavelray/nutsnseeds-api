6ee7acb77175305d3a7fb592f1a346e409f459e5
Pavel Ray <paray@lowes.com>
2021-05-03 15:30:12 +0530

    added user login and token generation in login

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,12 @@ const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXP
+  });
+};
+
 exports.signUp = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -11,9 +17,7 @@ exports.signUp = catchAsync(async (req, res, next) => {
     confirmPassword: req.body.password
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXP
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -29,8 +33,12 @@ exports.login = catchAsync(async (req, res, next) => {
 
   if (!email || !password) return next(new AppError('Please provide email and password for login', 400));
   const user = await User.findOne({ email }).select('+password');
-  const token = '';
 
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect Email or Password'), 401);
+  }
+
+  const token = signToken(user._id);
   res.status(200).json({
     status: 'success',
     token
--- models/userModel.js
+++ models/userModel.js
@@ -22,21 +22,26 @@ const userSchema = new mongoose.Schema({
   confirmPassword: {
     type: String,
     required: [true, 'Confirm password is required'],
-    validate:function(el){
+    validate: function(el) {
       return el === this.password;
     },
     message: 'Password and Confirm Password should match'
   }
 });
 
-userSchema.pre('save', async function(next){
-  if(!this.isModified('password')) return next();
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
 
-  this.password = await bcrypt.hash(this.password,12);
+  this.password = await bcrypt.hash(this.password, 12);
 
   this.confirmPassword = undefined;
   next();
-})
+});
+
+// This is an instance method. This will be availabe for all the instances of userModule
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
