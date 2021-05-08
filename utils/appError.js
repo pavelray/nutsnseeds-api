75ad8dfcf7e942882bd12bae9b994d2ece590ef0
unknown <pavelray@gmail.com>
2021-05-08 09:10:47 +0530

    Changes code comments

--- utils/appError.js
+++ utils/appError.js
@@ -4,7 +4,7 @@ class AppError extends Error {
 
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-    this.isOperational = true; // Here we are tracking only operational errors
+    this.isOperational = true; // Tracking only operational errors
 
     Error.captureStackTrace(this, this.constructor);
   }
