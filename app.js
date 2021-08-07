e44f97f205212aa11dc1ebb5065d53200b60b49a
Pavel Ray <35458270+pavelray@users.noreply.github.com>
2021-08-07 11:30:49 +0530

    Updated config

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
-const compression = require('compression');
 const timeout = require('connect-timeout');
 
 const AppError = require('./utils/appError');
@@ -52,7 +51,7 @@ app.use(xss());
 //   })
 // );
 
-app.use(compression);
+// app.use(compression);
 
 //Serving Static files
 app.use(express.static(`${__dirname}/public`));
